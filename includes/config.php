b5e75b9fab033d8654931add4e5437612c156460
Ubuntu <ubuntu@ip-172-31-13-24.ec2.internal>
2017-09-08 10:25:40 -0500

    Changed some default values:   includes/config.php

--- includes/config.php
+++ includes/config.php
@@ -8,16 +8,16 @@ define('DB_DATABASE', 'nflpickem');
 define('DB_PREFIX', 'nflp_');
 
 define('SITE_URL', 'http://localhost/personal/applications/phppickem/');
-define('ALLOW_SIGNUP', true);
-define('SHOW_SIGNUP_LINK', true);
+define('ALLOW_SIGNUP', false);
+define('SHOW_SIGNUP_LINK', false);
 define('USER_NAMES_DISPLAY', 3); // 1 = real names, 2 = usernames, 3 = usernames w/ real names on hover
 define('COMMENTS_SYSTEM', 'basic'); // basic, disqus, or disabled
 define('DISQUS_SHORTNAME', ''); // only needed if using Disqus for comments
 
 define('SEASON_YEAR', '2017');
 define('SERVER_TIMEZONE', 'America/Chicago'); // Your SERVER's timezone. NOTE: Game times will always be displayed in Eastern time, as they are on NFL.com. This setting makes sure cutoff times work properly.
-define('ALLWAYS_HIDE_PICKS', false); // Set to true to hide picks until games are locked out
-define('SHOW_TIEBREAKER_POINTS', false); // Set to true to use tiebreaker points, does not affect wins, mainly for bragging rights
+define('ALLWAYS_HIDE_PICKS', true); // Set to true to hide picks until games are locked out
+define('SHOW_TIEBREAKER_POINTS', true); // Set to true to use tiebreaker points, does not affect wins, mainly for bragging rights
 define('DEFAULT_TIEBREAKER_POINTS', 50); // if using tiebreaker points set default value to fill in on entry form
 
 // ***DO NOT EDIT ANYTHING BELOW THIS LINE***
